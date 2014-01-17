8cd50cc44b6520308ae5d783d0749872b991f5f1
Christopher A. Moore <chris.a.moore@gmail.com>
2014-01-17 14:45:43 -0800

    Add AWS CLI

--- puppet/manifests/default.pp
+++ puppet/manifests/default.pp
@@ -926,11 +926,24 @@ class bounce{
     }
 }
 
+class aws{
+    exec{ "install_pip":
+        command => "easy_install pip && pip install setuptools --no-use-wheel --upgrade",
+        returns => [ 0, 1, 255 ]
+    }
+
+    exec{ "install_aws_tools":
+        command => "pip install awscli",
+        returns => [ 0, 1, 255 ],
+        require => Exec["install_pip"],
+    }
+}
+
 class{ 'ohmyzsh': stage => nginx_setup }
 class{ 'nginx_setup': stage => nginx_setup }
 class{ 'packs': stage => nginx_setup }
 class{ 'redis': stage => nginx_setup }
-
+class{ 'aws': stage => main }
 class{ 'setup': stage => postsetup }
 
 class{ 'laravel_setup': stage => database }
